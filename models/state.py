2c0267ec6f534038d9358dde1a7eb85fb0a26183
R6Doc <fiendgear@gmail.com>
2020-09-02 19:56:42 -0500

    added db in state

--- models/state.py
+++ models/state.py
@@ -5,4 +5,20 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """get list of cities"""
+            citylist = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    citylist.append(city)
+            return citylist
